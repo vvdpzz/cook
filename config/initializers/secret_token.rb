fed031477481dd224efdc2be506afef2857eda0e
VvDPzZ <vvdpzz@gmail.com>
2010-09-03 17:53:45 +0800

    cook init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cook::Application.config.secret_token = '7b5638fdd2725a0c33daa0084bfd01daf67f9c76d54ec18e0ba86e813d303c92474d9a53db8aefe4f70ed5232294d936ac3115435c4d8744a27f535a2878c47c'
